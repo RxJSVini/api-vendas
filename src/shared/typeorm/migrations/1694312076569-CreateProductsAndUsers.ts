40659a3b9dcd2385416bccbb3b42c43460dce4cd
RxJSVini <viniciuspoa2@hotmail.com>
2023-09-09 22:00:10 -0500

    Terminando a parte de usuários

---
+++ src/shared/typeorm/migrations/1694312076569-CreateProductsAndUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateProductsAndUsers1694312076569 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
